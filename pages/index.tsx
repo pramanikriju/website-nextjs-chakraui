a09f29a2155189b501966d5b10b5fe0f1451b589
Riju Pramanik <pramanikriju@gmail.com>
2021-07-28 16:42:59 +0530

    landing page UI enhancements

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Hero from "../components/index/hero";
 import Feature from "../components/index/feature";
+import Stats from "../components/index/stats";
 
 export default function Home() {
   return (
@@ -7,6 +8,7 @@ export default function Home() {
       <Hero />
       <hr />
       <Feature />
+      <Stats />
     </div>
   );
 }
