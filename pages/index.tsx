fd101af0959a9b10596cd1112802d04911109fb0
Riju Pramanik <pramanikriju@gmail.com>
2021-07-30 13:29:13 +0530

    contact form WIP

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Hero from "../components/index/hero";
 import Feature from "../components/index/feature";
 import Stats from "../components/index/stats";
+import Contact from "../components/index/contact";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <hr />
       <Feature />
       <Stats />
+      <Contact />
     </div>
   );
 }
